cc0440c693e1fe344794c1ef73fd80632236e68a
Crisstuff <cristofferfolkedalrramsdal@gmail.com>
2025-02-27 11:27:29 +0100

    animasjon start av player

--- spillutvikling/tittle_screen.gd
+++ spillutvikling/tittle_screen.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_side.tscn")
+	get_tree().change_scene_to_file("res://World.tscn")
 	pass # Replace with function body.
 
 
