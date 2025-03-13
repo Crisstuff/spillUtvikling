67713d4c17c91f2ee40bc8ff5d10bffddbf2c8c4
m4dde <macaa015@osloskolen.no>
2025-03-13 12:19:54 +0100

    script til spikes

---
+++ spillutvikling/Scripts/spikes.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var speed = 160.0
+var current_speed = 0.0
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	
+
+func _on_player_detect_area_entered(area: Area2D) -> void:
+	
