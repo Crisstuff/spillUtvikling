fd775534391d3aff917371e90333193e89d720fa
m4dde <macaa015@osloskolen.no>
2025-03-17 15:08:42 +0100

    Update spikes.gd

--- spillutvikling/Scripts/spikes.gd
+++ spillutvikling/Scripts/spikes.gd
@@ -8,13 +8,17 @@ func _physics_process(delta):
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Player:
-		area.get_parent().die()
+		killPlayer(area.get_parent())
 
 func _on_player_detect_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Player:
 		print("Entered")
 		var tween = create_tween()
 		tween.tween_property(self, "global_position", Vector2(global_position.x, global_position.y + 500), 1)
+
+func killPlayer(player):
+	player.die()
+	print("Reset traps here")
 
 func fall():
 	current_speed = speed 
