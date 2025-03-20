a6540f89b2da43d30ca7f5ba7a09a23bf37efc5e
Crisstuff <cristofferfolkedalrramsdal@gmail.com>
2025-03-20 11:30:06 +0100

    view stach som jeg glemte og legge til

--- spillutvikling/Scripts/bullet.gd
+++ spillutvikling/Scripts/bullet.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+<<<<<<< Updated upstream
 var velocity: Vector2 = Vector2.ZERO
 
 func _process(delta):
@@ -7,3 +8,18 @@ func _process(delta):
 
 func set_velocity(new_velocity: Vector2):
 	velocity = new_velocity
+=======
+var speed = 300
+var direction = Vector2.RIGHT
+
+func _ready():
+	direction = direction.rotated(rotation)
+
+func _physics_process(delta):
+	position += direction * speed * delta
+
+func _on_Bullet_body_entered(body):
+	if body.name == "Player":
+		body.queue_free()
+	queue_free()
+>>>>>>> Stashed changes
--- spillutvikling/Scripts/pistol_cat.gd
+++ spillutvikling/Scripts/pistol_cat.gd
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 extends CharacterBody2D
 
 @onready var anim = get_node("AnimatedSprite2D")
@@ -36,3 +37,47 @@ func killPlayer(player):
 
 func _on_Area2D_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
+=======
+extends KinematicBody2D
+
+var player = null
+var speed = 100
+var direction = 1
+var bullet_speed = 300
+var bullet_scene = preload("res://bullet.tscn")
+
+func _ready():
+	$Timer.start()
+
+func _process(delta):
+	if player:
+		look_at(player.global_position)
+		if $Timer.is_stopped():
+			shoot()
+
+func _physics_process(delta):
+	move_and_slide(Vector2(direction * speed, 0))
+
+	if is_on_wall():
+		direction *= -1
+
+func _on_DetectionArea_body_entered(body):
+	if body.name == "Player":
+		player = body
+
+func _on_DetectionArea_body_exited(body):
+	if body.name == "Player":
+		player = null
+
+func shoot():
+	var bullet = bullet_scene.instance()
+	bullet.global_position = global_position
+	bullet.rotation = rotation
+	bullet.linear_velocity = Vector2.RIGHT.rotated(rotation) * bullet_speed
+	get_parent().add_child(bullet)
+	$Timer.start()
+
+func _on_Timer_timeout():
+	if player:
+		shoot()
+>>>>>>> Stashed changes
