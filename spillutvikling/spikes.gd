c4afae36a1a4b6004f2226c95c5d9b1324dd6a18
Crisstuff <cristofferfolkedalrramsdal@gmail.com>
2025-03-21 12:01:43 +0100

    spikes fix

--- spillutvikling/spikes.gd
+++ spillutvikling/spikes.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _on_area_2d_2_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Player:
+		get_tree().change_scene_to_file("res://Scenes/tittle_screen.tscn")
 		area.get_parent().die()
